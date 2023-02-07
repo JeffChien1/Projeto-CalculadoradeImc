b731e6546299c5652164991ba3f4175b83dd1d2a
Jeff Chien <eng.jeffchien@gmail.com>
2023-02-07 06:43:41 -0300

    JS: utilizando IF() para anunciar e invalidar pesos e alturas

--- js/main.js
+++ js/main.js
@@ -6,6 +6,24 @@ const tdimc = paciente.querySelector('.info-imc');
 
 var peso = tdpeso.textContent;
 var altura = tdaltura.textContent;
-var imc = peso / (altura * altura);
 
-tdimc.textContent = imc;
+var pesoEhValido = true;
+var alturaEhValido = true;
+
+
+if (peso < 0 || peso >= 1000) {
+    pesoEhValido = false;
+    tdimc.textContent = "Peso Inválido!!"
+}
+
+if (altura < 0 || altura >= 3.00) {
+    alturaEhValido = false;
+    tdimc.textContent = "Altura Inválido!!"
+}
+
+if (alturaEhValido && pesoEhValido) {
+    var imc = peso / (altura * altura);
+    tdimc.textContent = imc;
+}
+
+
