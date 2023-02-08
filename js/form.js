50e7be90f3fe3ae59693ebd648cbe4083872a0bb
Jeff Chien <eng.jeffchien@gmail.com>
2023-02-08 06:33:09 -0300

    Separando os arquivos em JS, aprimorando as boas praticas com JavaScript. Separando o arquivo que calcula imc, e adicionando um outro arquivo que trabalha com formulario criado no HTML

--- js/form.js
+++ js/form.js
@@ -1,36 +1,3 @@
-var pacientes = document.querySelectorAll('.paciente');
-
-console.log(paciente)
-
-for(var i = 0; i < pacientes.length; i++){
-
-    var paciente = pacientes[i];
-    const tdpeso = paciente.querySelector('.info-peso');
-    const tdaltura = paciente.querySelector('.info-altura');
-    const tdimc = paciente.querySelector('.info-imc');
-
-    var peso = tdpeso.textContent;
-    var altura = tdaltura.textContent;
-
-    var pesoEhValido = true;
-    var alturaEhValido = true;
-
-    if (peso < 0 || peso >= 1000) {
-        pesoEhValido = false;
-        tdimc.textContent = "Peso Inválido!!"
-    }
-    
-    if (altura < 0 || altura >= 3.00) {
-        alturaEhValido = false;
-        tdimc.textContent = "Altura Inválido!!"
-    }
-    
-    if (alturaEhValido && pesoEhValido) {
-        var imc = peso / (altura * altura);
-        tdimc.textContent = imc.toFixed(2);
-    }
-}
-
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 
 botaoAdicionar.addEventListener('click', (event) => {
@@ -69,4 +36,3 @@ botaoAdicionar.addEventListener('click', (event) => {
     tabela.appendChild(pacienteTr);
 
 })
-
