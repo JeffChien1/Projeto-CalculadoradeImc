471827851c6d93b3e671aac85be5b70b568ea1a9
Jeff Chien <eng.jeffchien@gmail.com>
2023-02-07 23:47:00 -0300

    HTML: Adicionando o formulario

--- js/main.js
+++ js/main.js
@@ -2,9 +2,6 @@ var pacientes = document.querySelectorAll('.paciente');
 
 console.log(paciente)
 
-
-
-
 for(var i = 0; i < pacientes.length; i++){
 
     var paciente = pacientes[i];
@@ -30,7 +27,46 @@ for(var i = 0; i < pacientes.length; i++){
     
     if (alturaEhValido && pesoEhValido) {
         var imc = peso / (altura * altura);
-        tdimc.textContent = imc.toFixed(3);
+        tdimc.textContent = imc.toFixed(2);
     }
 }
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    var form = document.querySelector('#form-adiciona');
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+    var imc = peso / (altura * altura);
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = imc.toFixed(2);
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+
+})
 
