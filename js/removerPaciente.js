c9002b4d9bbe5a5f0496e9f6e5528b4952137a76
Jeff Chien <eng.jeffchien@gmail.com>
2023-02-13 06:29:33 -0300

    JS: utilizando setTimeout para melhorar a transição e combinando com a classe do CSS na hora de deletar a linha da tabela

--- js/removerPaciente.js
+++ js/removerPaciente.js
@@ -3,11 +3,11 @@ var pacientes = document.querySelectorAll(".paciente");
 var tabela = document.querySelector("#tabela-pacientes");
 
 tabela.addEventListener("dblclick", function (event) {
-    var alvoEvento = event.target;
-    var paiDoAlvo = alvoEvento.parentNode;
+    event.target.parentNode.classList.add("fadeOut");
 
-    paiDoAlvo.remove();
-    
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    }, 500);
 })
 
 
