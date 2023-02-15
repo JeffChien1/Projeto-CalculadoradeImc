df65e82eb8904cd75cbacba275458887d2cf0cec
Jeff Chien <eng.jeffchien@gmail.com>
2023-02-15 06:52:55 -0300

    Utilizando a função adicionaPacienteNaTabela para adicionar os dados da API na tabela, utilizando JSON.parse para transformar os dados da API que são uma string em um array. Depois utiliza-se o forEach para passar por todos os dados da array e adicionando a função de adicionar o dado na tabela

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -4,13 +4,16 @@ var botaoAdicionar = document.querySelector("#buscar-pacientes");
 botaoAdicionar.addEventListener("click", function() {
     var xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "https://raw.githubusercontent.com/loresgarcia/Pacientes-API/master/pacientes.json");
+    xhr.open("GET", "https://raw.githubusercontent.com/loresgarcia/Pacientes-API/master/pacientes.json"); //json = JavaScript Object Notation
 
     xhr.addEventListener("load", function(){
-        console.log(xhr.responseText);
+        var resposta = xhr.responseText;
+        var pacientes = JSON.parse(resposta);
+
+        pacientes.forEach(function(paciente){
+            adicionaPacienteNaTabela(paciente);
+        });
     });
 
     xhr.send();
-
-
 });
